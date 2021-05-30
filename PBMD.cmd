5b39ba51c1e62cbd391d5beb67920cdada3dc40e
GabiBrawl <85069997+GabiBrawl@users.noreply.github.com>
2021-05-30 17:01:02 +0100

    Add files via upload

---
+++ PBMD.cmd
@@ -0,0 +1,26 @@
+echo off
+color f
+mode 27,6
+set counter=0
+set counterb=000
+set core=
+set "valuecore=               "
+:start
+cls
+echo.‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹
+echo.€%valuecore:~0,15%€
+echo.€‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹€
+if %counter% GEQ 15 set msg=complete& set counterb=10000& goto exit
+echo. %counterb:~0,-2%%%
+set /a counter=counter+1
+set /a counterb=counterb+666
+set valuecore=%%core:~0,%counter%%%
+for /f "usebackq tokens=*"  %%i in (`echo."%valuecore%"`) do set "valuecore=%%~i               "
+set delay=0
+:delay
+if %delay%==1 goto start
+set /a delay=delay+1
+goto delay
+:exit
+echo. %counterb:~0,-2%%% %msg%
+MD.cmd
---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+======================Bat-Chat V4.2======================
+			  BETA
+
+
+	This program only runs in Windows 7+.
+	In order to use this program with multiple computers install Google Drive sync in all the computers. Configure it with a new GMail account.
+Then paste the "Bat-Chat" folder anywhere in your drive folder. Run the "Chat" program in the computers you want to chat. Done! (v:
+	If you need something, email me at: gabrielyt219@gmail.com
