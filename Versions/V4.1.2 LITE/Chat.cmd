a3a7640f9a2281145b1bcb00e39e5e1b7de18d4b
Gabriel <85069997+GabiBrawl@users.noreply.github.com>
2021-07-10 12:51:52 +0100

    Update Chat.cmd

--- Versions/V4.1.2 LITE/Chat.cmd
+++ Versions/V4.1.2 LITE/Chat.cmd
@@ -4,9 +4,7 @@ title Command Prompt Chat
 color 07
 if not exist users goto creatediruser
 
-::--------------------
-::Below is the code to log-in to the program. 
-::--------------------
+
 :login
 title Command Prompt Chat LogIn
 color 07
@@ -35,32 +33,19 @@ echo Please type your password and press enter.
 echo.
 set /p password=Password:
 
-::Now we have the username and password set. We can now check to see if they are correct.
-::First we need to see if the username exists. 
 
 if exist ".\users\%username%.dll" goto password_check
-::If the username does not exist, we will now display
-::Incorrect Credentials Message and return to login
-:incorrect_credentials
 cls
 echo I'm sorry, but those credentials were not found. Please try again.
 timeout /t 3 >nul
 goto login
 
 
-::If the username did exist, we will now check to see if the password matches.
-:password_check
-
-::First, we need to get the password from the file and set it as a variable.
 set /p password_file=<".\users\%username%.dll"
-::Now, Compare the two
 if %password_file%==%password% goto correct_credentials
-
-::if they do not match, go again to incorrect credentials
 goto incorrect_credentials
 
-:create_account
-::Here we create an account. We need to ask for a username and password.
+
 cls
 echo _____________________
 echo Create an Account
@@ -82,7 +67,6 @@ timeout /t 3 >nul
 goto create_account
 
 :usr_password
-::Clear the screen, re-draw and ask for password
 cls
 echo _____________________
 echo Create an Account
@@ -92,12 +76,7 @@ echo.
 echo Please enter your information...
 echo.
 set /p new_password=Password:
-
-::Now that we have the information, we need to 
-::write it to the account file. We use the .dll extention
 echo %new_password% >".\users\%new_username%.dll"
-
-::now we confirm creation and go home
 echo.
 echo Account Successfully Created!
 timeout /t 2 >nul
@@ -106,7 +85,6 @@ goto login
 
 
 :correct_credentials
-::If credentials were correct, start up the message viewer and begin asking for input
 start cmd /c ".\PBMD.cmd"
 timeout /t 3
 title Chatting as %username%
@@ -136,32 +114,19 @@ color 1F
 
 cls
 mode con: cols=54 lines=4
-set /p input=Message:
-::If input is nothing, go back
+set /p input=Messages
 if "%input%"=="" goto read_messages
-::If input is /exit, exit the program.
 if "%input%"=="/exit" goto exit
-::If input is /cls, delete the history of messages.
 if "%input%"=="/cls" goto cls
-::If input is /cls -admin, delete the history of messages.
 if "%input%"=="/cls -admin" goto cls_admin
-::If input is /cls -admin -s, delete the history of messages.
 if "%input%"=="/cls -admin -s" goto cls_admin_s
-::If input is /logout, logout the user.
 if "%input%"=="/logout" goto logout
-::If input is /dcu, delete the current user.
 if "%input%"=="/dcu" goto dcu
-::If input is /help, open the help file.
 if "%input%"=="/help" goto help
-::If input is /rp, reset the password of the current user.
 if "%input%"=="/rp" goto passreset
-::If input is /md, run the message displayer.
 if "%input%"=="/md" start MD.cmd
 
-
-::Input message into chat file
 echo %username%: %input% >>chat.txt
-::reset the input to prevent spam
 set input=
 goto read_messages
 
